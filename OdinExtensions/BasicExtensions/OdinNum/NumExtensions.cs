5eb70e4e84755afa5d3a9b153efa0c457fbd7a71
odinsam <>
2021-07-08 18:43:54 +0800

    feat(OdinUtils.OdinExtensions): ✨ number 扩展

---
+++ OdinExtensions/BasicExtensions/OdinNum/NumExtensions.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace OdinPlugs.OdinUtils.OdinExtensions.BasicExtensions.OdinNum
+{
+    public class NumExtensions
+    {
+
+    }
+}
