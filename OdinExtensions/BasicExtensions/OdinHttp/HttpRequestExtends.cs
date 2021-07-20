16dc920d530f886eb67ea42669c7d631a5508975
‘odinsam’ <123679@qq.com>
2021-07-20 18:19:21 +0800

    ✨ feat(*): 新增获取 QueryString 的方法

--- OdinExtensions/BasicExtensions/OdinHttp/HttpRequestExtends.cs
+++ OdinExtensions/BasicExtensions/OdinHttp/HttpRequestExtends.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using System.IO;
 using Microsoft.AspNetCore.Http;
 
@@ -5,6 +6,23 @@ namespace OdinPlugs.OdinUtils.OdinExtensions.BasicExtensions.OdinNum.OdinHttp
 {
     public static class HttpRequestExtends
     {
+        /// <summary>
+        /// 获取 QueryString
+        /// </summary>
+        /// <param name="requestUri">请求的uri</param>
+        /// <returns>QueryString key-value 形式</returns>
+        public static Dictionary<string, string> GetRequestQueryString(this string requestUri)
+        {
+            string queryString = null;
+            if (requestUri.IndexOf('?') >= 0)
+                queryString = requestUri.Split('?')[1];
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            foreach (var item in queryString.Split('&'))
+            {
+                dic.Add(item.Split('=')[0], item.Split('=')[1]);
+            }
+            return dic;
+        }
         public static string ReadRequestBody(this HttpRequest request)
         {
             var reader = new StreamReader(request.Body);
